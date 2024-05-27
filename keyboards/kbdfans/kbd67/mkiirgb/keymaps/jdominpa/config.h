9e1a59ac9e09b8ddca3d6476fbd43ec649b4ac83
Joan Domingo Pasarin <jdomingopasarin@icloud.com>
2024-05-27 09:58:05 +0200

    Reimplemented one shot mods in NAV layer

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/jdominpa/config.h
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/jdominpa/config.h
@@ -3,7 +3,7 @@
 /* Mod tap */
 #define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
-#define QUICK_TAP_TERM 50
+#define QUICK_TAP_TERM 0
 
 /* Caps word */
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/jdominpa/keymap.c
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/jdominpa/keymap.c
@@ -1,7 +1,9 @@
 #include QMK_KEYBOARD_H
 
-#define OS_LSFT OSM(MOD_LSFT)
-#define OS_RSFT OSM(MOD_RSFT)
+#define OS_SHFT OSM(MOD_LSFT)
+#define OS_CTRL OSM(MOD_LCTL)
+#define OS_ALT OSM(MOD_LALT)
+#define OS_GUI OSM(MOD_LGUI)
 #define TAB_L C(KC_TAB)
 #define TAB_R C(S(KC_TAB))
 #define LA_NAV LT(NAV, KC_SPC)
@@ -14,17 +16,17 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[DEF] = LAYOUT_65_ansi_blocker( /* Default */
-		KC_GRV,   KC_1,          KC_2,          KC_3,          KC_4,          KC_5,    KC_6,   KC_7,          KC_8,          KC_9,           KC_0,              KC_MINS,  KC_EQL,  KC_ESC,    KC_DEL,
+		KC_GRV,   KC_1,          KC_2,          KC_3,          KC_4,          KC_5,    KC_6,   KC_7,          KC_8,          KC_9,           KC_0,              KC_MINS,  KC_EQL,  KC_BSPC,   KC_DEL,
 		KC_TAB,   KC_Q,          KC_W,          KC_E,          KC_R,          KC_T,    KC_Y,   KC_U,          KC_I,          KC_O,           KC_P,              KC_LBRC,  KC_RBRC, KC_BSLS,   XXXXXXX,
-		KC_BSPC,  LGUI_T(KC_A),  LALT_T(KC_S),  LCTL_T(KC_D),  LSFT_T(KC_F),  KC_G,    KC_H,   RSFT_T(KC_J),  RCTL_T(KC_K),  RALT_T(KC_L),   RGUI_T(KC_SCLN),   KC_QUOT,           KC_ENT,    TG(GAME),
-		OS_LSFT,  KC_Z,          KC_X,          KC_C,          KC_V,          KC_B,    KC_N,   KC_M,          KC_COMM,       KC_DOT,         KC_SLSH,           OS_RSFT,           KC_UP,     XXXXXXX,
+		KC_ESC,   LGUI_T(KC_A),  LALT_T(KC_S),  LCTL_T(KC_D),  LSFT_T(KC_F),  KC_G,    KC_H,   RSFT_T(KC_J),  RCTL_T(KC_K),  RALT_T(KC_L),   RGUI_T(KC_SCLN),   KC_QUOT,           KC_ENT,    TG(GAME),
+		OS_SHFT,  KC_Z,          KC_X,          KC_C,          KC_V,          KC_B,    KC_N,   KC_M,          KC_COMM,       KC_DOT,         KC_SLSH,           OS_SHFT,           KC_UP,     XXXXXXX,
 		KC_LCTL,  KC_LGUI,       KC_LALT,                                     LA_NAV,                         KC_RALT,                       KC_RCTL,           KC_LEFT,           KC_DOWN,   KC_RIGHT),
 
 	[NAV] = LAYOUT_65_ansi_blocker( /* Navigation and editing */
 		XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  QK_BOOT,
 		XXXXXXX,  KC_TAB,   KC_WSCH,  TAB_L,    TAB_R,    KC_VOLU,  KC_PGUP,  KC_HOME,  KC_UP,    KC_END,   KC_DEL,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-		XXXXXXX,  KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  KC_VOLD,  KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_BSPC,  XXXXXXX,            XXXXXXX,  XXXXXXX,
-		XXXXXXX,  KC_MSTP,  KC_MUTE,  KC_MPRV,  KC_MNXT,  KC_MPLY,  KC_UNDO,  KC_CUT,   KC_COPY,  KC_PSTE,  KC_ENT,   XXXXXXX,            XXXXXXX,  XXXXXXX,
+		XXXXXXX,  OS_GUI,   OS_ALT,   OS_CTRL,  OS_SHFT,  KC_VOLD,  KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_BSPC,  XXXXXXX,            XXXXXXX,  XXXXXXX,
+		XXXXXXX,  KC_MUTE,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MSTP,  KC_UNDO,  KC_CUT,   KC_COPY,  KC_PSTE,  KC_ENT,   XXXXXXX,            XXXXXXX,  XXXXXXX,
 		XXXXXXX,  XXXXXXX,  XXXXXXX,                      XXXXXXX,                      XXXXXXX,            XXXXXXX,  XXXXXXX,            XXXXXXX,  XXXXXXX),
 
 	[GAME] = LAYOUT_65_ansi_blocker( /* Gaming */
